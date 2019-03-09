4bc7c6071b926371b1e45c237bf808551fe175d1
timgrohmann <timgrohmann@icloud.com>
2019-03-09 11:39:04 +0100

    v2

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -3,7 +3,10 @@
     <v-card-title>
       <div class="headline">Hier könnten Dinge stehen!</div>
     </v-card-title>
-    <v-card-text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, ad. Natus, reiciendis commodi doloribus laudantium mollitia maxime nulla dolore libero voluptatum necessitatibus, dolorem fugiat at neque hic, eaque fugit nemo.</v-card-text>
+    <v-card-text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Nesciunt, ad. Natus, reiciendis commodi doloribus laudantium mollitia
+      maxime nulla dolore libero voluptatum necessitatibus, dolorem fugiat
+      at neque hic, eaque fugit nemo. v2</v-card-text>
   </v-card>
 </template>
 
