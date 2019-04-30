d9f89af477c1b0b545113241825a82c9d35b9750
timgrohmann <timgrohmann@icloud.com>
2019-04-30 15:25:48 +0200

    added first working integrationdisplay all entries on landing page

--- vue-frontend/src/index.js
+++ vue-frontend/src/index.js
@@ -23,21 +23,7 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 });
 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-var params = {
-  TableName: 'exwm-entries',
-  Key: {
-    'EntryId' : {N: '1'}
-  }
-};
-
-// Call DynamoDB to add the item to the table
-ddb.getItem(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data);
-  }
-});
+store.commit("setDB", ddb)
 
 new Vue({
   store,
