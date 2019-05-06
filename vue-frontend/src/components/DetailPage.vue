56734158cc1aec06593c0385faf1f71d45e21430
timgrohmann <timgrohmann@icloud.com>
2019-05-06 21:12:44 +0200

    rebased to master

--- vue-frontend/src/components/DetailPage.vue
+++ vue-frontend/src/components/DetailPage.vue
@@ -4,12 +4,21 @@
       <div class="headline">{{item.headline}}</div>
     </v-card-title>
     <v-card-text v-html="markedHtml"></v-card-text>
+    <v-card-text>{{item.score}}</v-card-text>
+    <v-card-actions>
+      <v-btn outline round color="success" v-on:click="upvote">
+        <v-icon>thumb_up</v-icon>&nbsp{{item.upvotes}}
+      </v-btn>
+      <v-btn outline round color="red">
+        <v-icon>thumb_down</v-icon>&nbsp{{item.downvotes}}
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
-import db from "../data"
 import marked from "marked"
+import data from "../data"
 
 export default {
   props: {
@@ -36,13 +45,24 @@ export default {
   },
   methods: {
     refresh() {
-      db.findByUUID(this.uuid, (error, data) => {
+      data.findByUUID(this.uuid, (error, data) => {
         console.log(data)
         if (data.Items.length == 1) {
+          this.item.uuid = data.Items[0].uuid.S
+          this.item.timestamp = data.Items[0].timestamp.S
           this.item.headline = data.Items[0].headline.S
           this.item.body = data.Items[0].body.S
+          this.item.upvotes = parseInt(data.Items[0].upvotes.N)
+          this.item.downvotes = data.Items[0].downvotes.N
         }
       })
+    },
+    upvote() {
+      data.updateUpvotes(this.item, this.item.upvotes+1, (error, data) => {
+        console.log(error);
+      })
+      this.item.upvotes++
+      console.log(this.item.upvotes)
     }
   },
   computed: {
