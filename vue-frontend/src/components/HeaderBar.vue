ae262644857dbceff50f675dd358f54d7d61722b
Felix Wortmann <felixwortmann@pm.me>
2019-05-10 12:22:34 +0200

    rebased with master and merged

--- vue-frontend/src/components/HeaderBar.vue
+++ vue-frontend/src/components/HeaderBar.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <div>
     <v-navigation-drawer app v-model="visible" mobile-break-point="10000">
       <v-list>
@@ -18,22 +18,92 @@
       <router-link :to="{name: 'LandingPage'}" class="v-responsive" style="max-height: 100%">
         <v-img src="ewm.svg" max-height="100%" position="left center" contain/>
       </router-link>
+
+
+      <v-spacer></v-spacer>
+      <v-autocomplete
+        v-model="model"
+        :items="entries"
+        :search-input.sync="search"
+        clearable
+        hide-details
+        hide-selected
+        item-text="name"
+        item-value="symbol"
+        label="Suche nach einem Eintrag..."
+        solo
+
+      >
+        <template v-slot:no-data>
+          <v-list-tile>
+            <v-list-tile-title>
+              Suche nach einem Eintrag
+            </v-list-tile-title>
+          </v-list-tile>
+        </template>
+        <!--        <template v-slot:selection="{ item, selected }">-->
+        <!--          <v-chip-->
+        <!--            :selected="selected"-->
+        <!--            color="blue-grey"-->
+        <!--            class="white&#45;&#45;text"-->
+        <!--          >-->
+        <!--            <v-icon left>mdi-coin</v-icon>-->
+        <!--            <span v-text="item.headline.S"></span>-->
+        <!--          </v-chip>-->
+        <!--        </template>-->
+        <template v-slot:item="{ item }">
+          <v-list-tile @click="model=false" :to="{name: 'DetailPage', params: {id: item.uuid.S}}">
+            <v-list-tile-avatar
+              color="indigo"
+              class="headline font-weight-light white--text"
+            >
+              {{ item.headline.S.charAt(0) }}
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.headline.S"></v-list-tile-title>
+              <v-list-tile-sub-title v-text="item.body.S"></v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <!--          <v-list-tile-action>-->
+          <!--            <v-icon>mdi-coin</v-icon>-->
+          <!--          </v-list-tile-action>-->
+        </template>
+      </v-autocomplete>
     </v-toolbar>
   </div>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    items: [
-      { icon: "face", name: "Über uns", link: { name: "AboutUs" } },
-      { icon: "search", name: "Suche" },
-      { icon: "add", name: "Neuer Eintrag", link: { name: "CreateEntry" } },
-      { icon: "list", name: "Alle Beiträge", link: { name: "AllEntries" } },
-    ],
-    visible: false
-  })
-}
+  import db from "../data"
+
+  export default {
+    data: () => ({
+      search: null,
+      isLoading: false,
+      model: null,
+      items: [
+        {icon: "face", name: "Über uns", link: {name: "AboutUs"}},
+        {icon: "search", name: "Suche"},
+        {icon: "add", name: "Neuer Eintrag", link: {name: "CreateEntry"}},
+        {icon: "list", name: "Alle Beiträge", link: {name: "AllEntries"}},
+      ],
+      visible: false,
+      entries: [],
+    }),
+    mounted() {
+      db.getAll((err, data) => {
+        if (err) {
+          console.error("Error while scanning database:", err)
+        } else {
+          this.entries = data.Items
+        }
+      })
+    },
+
+
+  }
+
 </script>
 
 <style>
