f46e53b81ef042d83f64dd0677a4c562caf8be77
Nico Oberländer <nicoberlaender@gmail.com>
2019-05-10 15:25:11 +0200

    Yes

--- vue-frontend/src/components/DetailPage.vue
+++ vue-frontend/src/components/DetailPage.vue
@@ -1,23 +1,49 @@
 <template>
-  <v-card>
-    <v-card-title>
-      <div class="headline">{{item.headline}}</div>
-    </v-card-title>
-    <v-card-text v-html="markedHtml"></v-card-text>
-    <v-card-actions>
-      <v-btn outline round color="success" @click="upvote">
-        <v-icon>thumb_up</v-icon>
-        &nbsp;{{item.upvotes}}
-      </v-btn>
-      <v-btn outline round color="error" @click="downvote">
-        <v-icon>thumb_down</v-icon>
-        &nbsp;{{item.downvotes}}
-      </v-btn>
-      <v-btn outline color="primary">
-        <a style="text-decoration:none" :href="'mailto:'+item.email+'?subject=Rückmeldung zu: '+item.headline">Rückmeldung geben</a>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <div>
+    <v-card>
+      <v-card-title>
+        <div class="headline">{{item.headline}}</div>
+      </v-card-title>
+      <v-card-text v-html="markedHtml"></v-card-text>
+      <v-card-actions>
+        <v-btn outline round color="success" @click="upvote">
+          <v-icon>thumb_up</v-icon>
+          &nbsp;{{item.upvotes}}
+        </v-btn>
+        <v-btn outline round color="error" @click="downvote">
+          <v-icon>thumb_down</v-icon>
+          &nbsp;{{item.downvotes}}
+        </v-btn>
+        <v-btn outline color="primary">
+          <a
+            style="text-decoration:none"
+            :href="'mailto:'+item.email+'?subject=Rückmeldung zu: '+item.headline"
+          >Rückmeldung geben</a>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <br>
+    <v-layout justify-center>
+      <v-card width="50%" flat img="null">
+        <v-card-title>
+          <div class="headline">Kommentar hinzufügen:</div>
+        </v-card-title>
+        <v-text-field solo label="Dein Name" v-model="headline"></v-text-field>
+        <v-textarea
+          v-model="commentBody"
+          label="Kommentar"
+          hint="Hier kannst du den Beitrag kommentieren"
+          @input="tag_text(commentBody)"
+          solo
+        ></v-textarea>
+        <v-card-actions>
+          <v-btn fab small color="primary" absolute top right>
+            <v-icon>send</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -35,7 +61,7 @@ export default {
     return {
       item: {
         headline: "…",
-        body: "…",
+        body: "…"
       }
     }
   },
