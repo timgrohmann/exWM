4fdcfdc068c8b06ef49a3fc6cc250c6fb27f67d0
Florian Redmann <florian.redmann@rewe-group.com>
2019-05-13 13:36:44 +0200

    Fixed console error in DeleteConfirmation

--- vue-frontend/src/components/DeleteConfirmation.vue
+++ vue-frontend/src/components/DeleteConfirmation.vue
@@ -3,7 +3,6 @@
     <v-card-title>
       <div class="headline">Beitrag erfolgreich gelöscht</div>
     </v-card-title>
-    <v-card-text v-html="markedHtml"></v-card-text>
     <v-card-actions>
       <v-btn outline color="primary" :to="{name: 'LandingPage'}">
         Zurück zur Übersicht
