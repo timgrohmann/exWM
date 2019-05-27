4ce8720e89ee5e4dfc352aed3a11ccec8f9315bd
Felix <felixwortmann@protonmail.com>
2019-05-27 14:27:00 +0200

    fixed keyboard issue

--- mobile_flutter_exwm/lib/main.dart
+++ mobile_flutter_exwm/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,32 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    WebViewController _controller;
-    return SafeArea(
-          child: WebView(
-        javascriptMode: JavascriptMode.unrestricted,
-        initialUrl: 'https://exwm.timgrohmann.de',
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller = webViewController;
-        },
-      ),
+      routes: {
+        "/": (_) => SafeArea(
+              child: WebviewScaffold(
+                url: "https://exwm.timgrohmann.de/",
+              ),
+            ),
+      },
     );
   }
 }
